0899c8fe4a2731adc12346f3293d874b9b9a75ab
Kangqi Kenny Fu <46585854+KennyKangMPC@users.noreply.github.com>
2020-01-11 17:55:42 -0600

    Add files via upload

--- Deep Learning/randGen.m
+++ Deep Learning/randGen.m
@@ -1,19 +1,30 @@
 %{
+Subroutine generate random list of 0-1
 ni: sample size
 ns: number of source
-d : density (minimum number of ones)
+minD : density (minimum number of ones)
 %}
-function ranList = randGen(ni,ns,d)
+
+%random_x = x(randperm(size(x, 1)), :); randomly shuffle rows
+
+function ranList = randGen(ni,ns,minD)
 pos = 1:ns;
 ranList = [];
-idx = 1;
+idx = minD;
 while 1
     cN = combnk(pos, idx);
-    r = randperm(randi(length(cN),1));
-    for ri = r
-        idx = zeros(1,ns);
-        idx(cN(ri,:)) = 1;
-        ranList = [ranList;idx];
+    r = randperm(randi(size(cN,1),1));
+    if size(ranList,1) >= ni
+        break
+    elseif length(r) > ni || (length(r) + size(ranList,1) > ni)
+        continue
+    else
+        for ri = r
+            idxs = zeros(1,ns);
+            disp(ri)
+            idxs(cN(ri,:)) = 1;
+            ranList = [ranList;idxs];
+        end
     end
     idx = idx + 1;
 end
--- Deep Learning/randImage.m
+++ Deep Learning/randImage.m
@@ -1,20 +1,19 @@
 %{
 This code is to generate random square source location and then plot
-them.
+them. Here is the main routine
 %}
 close all
 clear;
 clc;
 
 %% Declare parameter
-%x = input('source width: ');
-%y = input('source length: ');
-%d = input('distance between two sources: ');
-ro = input('overlaps density: ');
+x = input('source width: ');
+y = input('source length: ');
+d = input('distance between two sources: ');
+ro = input('overlaps starting density: ');
 n = input('number of source: ');
 ni = input('sample size: ');
-
 %% Generate list of random 0-1 (guarantee no repetitive)
 randPool = randGen(ni, n, ro);
 %% Plot and save images for later training
-%imCreater(randPool, x, y, d, h)
+imCreater(randPool, x, y, d, h);

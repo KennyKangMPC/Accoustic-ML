22e2a997fa00dd354efed4d209ce5a9f9b175c32
Kangqi Kenny Fu <46585854+KennyKangMPC@users.noreply.github.com>
2020-01-12 14:18:09 -0600

    Add files via upload

--- Deep Learning/randGen.m
+++ Deep Learning/randGen.m
@@ -6,7 +6,7 @@ minD : density (minimum number of ones)
 %}
 
 %random_x = x(randperm(size(x, 1)), :); randomly shuffle rows
-
+%{
 function ranList = randGen(ni,ns,minD)
 pos = 1:ns;
 ranList = [];
@@ -29,3 +29,26 @@ while 1
     idx = idx + 1;
 end
 end
+%}
+
+% This algorithm works, but worse then the one above.
+% This algorithm actually is pretty fast lmao
+function ranList = randGen(ni,ns,minD)
+pos = 1:ns;
+ranList = [];
+for i = minD:ns
+    cN = combnk(pos, i);
+    for j = 1:size(cN,1)
+        idxs = zeros(1,ns);
+        idxs(cN(j,:)) = 1;
+        ranList = [ranList;idxs];
+    end
+end
+
+temp = ranList(randperm(size(ranList, 1)), :); % rand shuffle
+if size(ranList,1) > ni
+    ranList = temp(1:ni,:);
+end
+end
+    
+
